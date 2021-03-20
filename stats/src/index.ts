814649b065e28104e0626a6ca46f4b780083d42a
zakiafada32 <afadazaki32@gmail.com>
2021-03-20 19:25:12 +0700

    report

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,5 +1,17 @@
-import { CsvFileReader } from './csvFileReader';
+import { MatchReader } from './MatchReader';
+import { Summary } from './Summary';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
 
-const reader = new CsvFileReader('./football.csv');
-reader.read();
-console.log(reader.data);
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
+
+matchReader.load();
+summary.buildAndPrintReport(matchReader.matches);
+
+const consoleSummary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+);
+
+consoleSummary.buildAndPrintReport(matchReader.matches);
