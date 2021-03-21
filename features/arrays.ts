e65ee09b59fc6aad6566bcf47324fca438e7b160
zakiafada32 <afadazaki32@gmail.com>
2021-03-21 12:07:19 +0700

    export

--- features/arrays.ts
+++ features/arrays.ts
@@ -1,3 +1,4 @@
+export {};
 const carMakers = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 
@@ -8,7 +9,7 @@ const car = carMakers[0];
 const myCar = carMakers.pop();
 
 // Prevent incompatible values
-carMakers.push(100);
+// carMakers.push(100);
 
 // Help with 'map'
 carMakers.map((car: string): string => {
