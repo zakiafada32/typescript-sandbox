0c1bfa02880c4b0dbc3294ddb1116b347deb672a
zakiafada32 <afadazaki32@gmail.com>
2021-03-24 02:03:34 +0700

    getter setter

--- framework/src/index.ts
+++ framework/src/index.ts
@@ -1,5 +1,17 @@
 import { User } from './models/User';
 
+const user = new User({ name: 'A', age: 1 });
+
+user.on('change', () => {
+  console.log('User was changed');
+});
+
+user.set({ name: 'new name' });
+
 const user2 = new User({ id: 1 });
+
+user2.on('change', () => {
+  console.log(user2);
+});
+
 user2.fetch();
-user2.events.
--- framework/src/models/User.ts
+++ framework/src/models/User.ts
@@ -1,5 +1,7 @@
+import { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 export interface UserProps {
   id?: number;
   name?: string;
@@ -11,4 +13,37 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attribute: Attributes<UserProps>;
+
+  constructor(attr: UserProps) {
+    this.attribute = new Attributes<UserProps>(attr);
+  }
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attribute.get;
+  }
+
+  set(update: UserProps): void {
+    this.attribute.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+    console.log(id);
+    if (typeof id !== 'number') {
+      throw new Error('cannot fetch data without id');
+    }
+    this.sync.fetch(id).then((res: AxiosResponse): void => {
+      this.set(res.data);
+    });
+  }
 }
