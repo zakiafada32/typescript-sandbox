098f5102300d03c8f5056a26a94ba635a31e9cc5
zakiafada32 <afadazaki32@gmail.com>
2021-03-28 10:21:04 +0700

    set name

--- framework/src/index.ts
+++ framework/src/index.ts
@@ -1,5 +1,8 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const userForm = new UserForm(document.getElementById('root')!);
+const user = User.buildUser({ name: 'Abc', age: 123 });
+
+const userForm = new UserForm(document.getElementById('root')!, user);
 
 userForm.render();
--- framework/src/views/UserForm.ts
+++ framework/src/views/UserForm.ts
@@ -1,37 +1,53 @@
+import { User } from '../models/User';
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick,
-      'mouseenter:h1': this.onHoverH1,
+      'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
     };
   }
 
-  onHoverH1(): void {
-    console.log('h1 hover');
-  }
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+    if (input) {
+      const name = input.value;
+      this.model.set({ name });
+    }
+  };
 
-  onButtonClick(): void {
-    console.log('click button');
-  }
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
 
   template(): string {
     return `
     <div>
       <h1>User Form</h1>
+      <div>Name: ${this.model.get('name')}</div>
+      <div>Age: ${this.model.get('age')}</div>
       <input />
-      <button>Update</button>
+      <button class="set-name">Change Name</button>
+      <button class="set-age">Set Random Age</button>
     </div>
   `;
   }
 
   bindEvent(fragment: DocumentFragment): void {
+    this.parent.innerHTML = '';
     const eventMap = this.eventsMap();
     for (const eventKey in eventMap) {
       const [eventName, selector] = eventKey.split(':');
-      console.log(eventName, selector);
-
       fragment.querySelectorAll(selector).forEach((element) => {
         element.addEventListener(eventName, eventMap[eventKey]);
       });
